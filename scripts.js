d81ae0d106c172aaf3e27c689dc6f9f35a9aff60
Zane Woodson <59838229+zanewoodson@users.noreply.github.com>
2020-05-11 16:34:56 -0600

    Added Heading Animation Section

--- scripts.js
+++ scripts.js
@@ -38,13 +38,17 @@ const video = intro.querySelector('#heroIntroAnimation');
 const heroHeadline = intro.querySelector('.hero-headline');
 const heroDescription = intro.querySelector('.copy1');
 const features = intro.querySelector('.copy2');
-
 const section = document.querySelector('#indroduction');
 const end = document.querySelector('#noVelocity');
+
+
+const headingAnimation = document.querySelector('.heading-image-sequence-component');
+const headingGizmaAnimation = document.querySelector('#headingGizmoAnimation');
 
 //SCROLLMAGIC
 const controller = new ScrollMagic.Controller();
 
+// Intro Image Sequence //
 let heroImages = [
     "images/hero-animation0001.png",
     "images/hero-animation0002.png",
@@ -167,7 +171,6 @@ let scene = new ScrollMagic.Scene({
   triggerElement: intro,
   triggerHook: 0
 })
-.addIndicators()
 .setTween(tween)
 .setPin(intro)
 .addTo(controller);
@@ -198,3 +201,155 @@ let scene3 = new ScrollMagic.Scene({
 })
 .setTween(introLine)
 .addTo(controller);
+
+
+// Heading Image Sequence
+let headingImages = [
+    "images/Heading0001.png",
+    "images/Heading0002.png",
+    "images/Heading0003.png",
+    "images/Heading0004.png",
+    "images/Heading0005.png",
+    "images/Heading0006.png",
+    "images/Heading0007.png",
+    "images/Heading0008.png",
+    "images/Heading0009.png",
+    "images/Heading0010.png",
+    "images/Heading0011.png",
+    "images/Heading0012.png",
+    "images/Heading0013.png",
+    "images/Heading0014.png",
+    "images/Heading0015.png",
+    "images/Heading0016.png",
+    "images/Heading0017.png",
+    "images/Heading0018.png",
+    "images/Heading0019.png",
+    "images/Heading0020.png",
+    "images/Heading0021.png",
+    "images/Heading0022.png",
+    "images/Heading0023.png",
+    "images/Heading0024.png",
+    "images/Heading0025.png",
+    "images/Heading0026.png",
+    "images/Heading0027.png",
+    "images/Heading0028.png",
+    "images/Heading0029.png",
+    "images/Heading0030.png",
+    "images/Heading0031.png",
+    "images/Heading0032.png",
+    "images/Heading0033.png",
+    "images/Heading0034.png",
+    "images/Heading0035.png",
+    "images/Heading0036.png",
+    "images/Heading0037.png",
+    "images/Heading0038.png",
+    "images/Heading0039.png",
+    "images/Heading0040.png",
+    "images/Heading0041.png",
+    "images/Heading0042.png",
+    "images/Heading0043.png",
+    "images/Heading0044.png",
+    "images/Heading0045.png",
+    "images/Heading0046.png",
+    "images/Heading0047.png",
+    "images/Heading0048.png",
+    "images/Heading0049.png",
+    "images/Heading0050.png",
+	];
+
+  // Heading Image Sequence
+  let headingGizmoImages = [
+      "images/heading-gizmo_00000.png",
+      "images/heading-gizmo_00001.png",
+      "images/heading-gizmo_00002.png",
+      "images/heading-gizmo_00003.png",
+      "images/heading-gizmo_00004.png",
+      "images/heading-gizmo_00005.png",
+      "images/heading-gizmo_00006.png",
+      "images/heading-gizmo_00007.png",
+      "images/heading-gizmo_00008.png",
+      "images/heading-gizmo_00009.png",
+      "images/heading-gizmo_00010.png",
+      "images/heading-gizmo_00011.png",
+      "images/heading-gizmo_00012.png",
+      "images/heading-gizmo_00013.png",
+      "images/heading-gizmo_00014.png",
+      "images/heading-gizmo_00015.png",
+      "images/heading-gizmo_00016.png",
+      "images/heading-gizmo_00017.png",
+      "images/heading-gizmo_00018.png",
+      "images/heading-gizmo_00019.png",
+      "images/heading-gizmo_00020.png",
+      "images/heading-gizmo_00021.png",
+      "images/heading-gizmo_00022.png",
+      "images/heading-gizmo_00023.png",
+      "images/heading-gizmo_00024.png",
+      "images/heading-gizmo_00025.png",
+      "images/heading-gizmo_00026.png",
+      "images/heading-gizmo_00027.png",
+      "images/heading-gizmo_00029.png",
+      "images/heading-gizmo_00030.png",
+      "images/heading-gizmo_00031.png",
+      "images/heading-gizmo_00032.png",
+      "images/heading-gizmo_00033.png",
+      "images/heading-gizmo_00034.png",
+      "images/heading-gizmo_00035.png",
+      "images/heading-gizmo_00036.png",
+      "images/heading-gizmo_00037.png",
+      "images/heading-gizmo_00038.png",
+      "images/heading-gizmo_00039.png",
+      "images/heading-gizmo_00040.png",
+      "images/heading-gizmo_00041.png",
+      "images/heading-gizmo_00042.png",
+      "images/heading-gizmo_00043.png",
+      "images/heading-gizmo_00044.png",
+      "images/heading-gizmo_00045.png",
+      "images/heading-gizmo_00046.png",
+      "images/heading-gizmo_00047.png",
+      "images/heading-gizmo_00048.png",
+  	];
+
+  let headingObj = {curImg: 0};
+  let headingGizObj = {curImg: 0};
+
+  let headingAnima = TweenMax.to(headingObj, 0.5,
+  		{
+  			curImg: headingImages.length - 1,	// animate propery curImg to number of images
+  			roundProps: "curImg",				// only integers so it can be used as an array index
+  			repeat: 0,									// repeat 3 times
+  			immediateRender: true,			// load first image automatically
+  			ease: Linear.easeNone,			// show every image the same ammount of time
+  			onUpdate: function () {
+  			  $("#headingAnimation").attr("src", headingImages[headingObj.curImg]); // set the image source
+  			}
+  		}
+  	);
+
+    let headingGizmo = TweenMax.to(headingGizObj, 0.5,
+    		{
+    			curImg: headingGizmoImages.length - 1,	// animate propery curImg to number of images
+    			roundProps: "curImg",				// only integers so it can be used as an array index
+    			repeat: 0,									// repeat 3 times
+    			immediateRender: true,			// load first image automatically
+    			ease: Linear.easeNone,			// show every image the same ammount of time
+    			onUpdate: function () {
+    			  $("#headingGizmoAnimation").attr("src", headingGizmoImages[headingGizObj.curImg]); // set the image source
+    			}
+    		}
+    	);
+
+    let scene4 = new ScrollMagic.Scene({
+      duration: 600,
+      triggerElement: headingAnimation,
+      triggerHook: 0.5
+    })
+    .setTween(headingAnima)
+    .addTo(controller);
+
+    let scene5 = new ScrollMagic.Scene({
+      duration: 500,
+      triggerElement: headingAnimation,
+      triggerHook: 0.5
+    })
+    .setTween(headingGizmo)
+    .addTo(controller);
